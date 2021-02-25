9a5634512c62478bb3a8dcf08bb684a007f108cc
sohui2470 <sohui2470@gmail.com>
2021-02-25 23:24:09 +0900

    jump to java practices

---
+++ Thread01.java
@@ -0,0 +1,25 @@
+package java_space;
+
+public class Thread01 extends Thread{
+	int seq;
+	public Thread01 (int seq) {
+		this.seq = seq;
+	}
+	public void run() {
+		System.out.println(this.seq+" thread start.");
+		try {
+			Thread.sleep(1000);
+		} catch(Exception e) {
+			
+		}
+		System.out.println(this.seq+" thread end.");
+	}
+	
+	public static void main(String[] args) {
+		for (int i=0; i<10; i++) {
+			Thread t =  new Thread01(i);
+			t.start();
+		}
+		System.out.println("main end");
+	}
+}
